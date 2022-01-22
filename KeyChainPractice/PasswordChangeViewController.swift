7af34a5d519343b93db763ab384e42e8f3efa619
Jager-yoo <y73447jh@gmail.com>
2022-01-23 03:51:07 +0900

    feat: textFieldDidChangeSelection 메서드를 활용해서 textField 내용 유무에 따라 버튼이 활성화되는 기능 구현
    - 만약 변경할 비밀번호가 공백()이라면, 비밀번호를 Keychain 에서 삭제시키는 로직 추가
    - 비밀번호가 공백()인 경우, currentPasswordTextField 의 placeholder 가 nil 이 나와서 크래시나는 이슈 해결

--- KeyChainPractice/PasswordChangeViewController.swift
+++ KeyChainPractice/PasswordChangeViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class PasswordChangeViewController: UIViewController {
+class PasswordChangeViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet private weak var currentPasswordTextField: UITextField!
     @IBOutlet private weak var newPasswordTextField: UITextField!
@@ -15,12 +15,22 @@ class PasswordChangeViewController: UIViewController {
     
     @IBAction private func changePassword(_ sender: UIButton) {
         
-        guard newPasswordTextField.text! != currentPasswordTextField.placeholder! else {
+        guard newPasswordTextField.text! != currentPasswordTextField.placeholder ?? "" else {
             showBasicAlert(title: "기존 비밀번호와 중복입니다.", message: "새로운 비밀번호를 등록해주세요!", handler: nil)
             return
         }
         
         let query: [String: Any] = [kSecClass as String: kSecClassGenericPassword]
+        
+        // 만약 변경할 비밀번호가 공백("") 이면, 비밀번호 삭제시키는 로직
+        guard newPasswordTextField.text!.isEmpty == false else {
+            let _ = SecItemDelete(query as CFDictionary)
+            showBasicAlert(title: "비밀번호가 삭제됐습니다.", message: "새로운 비밀번호를 다시 등록해주세요!") { _ in
+                self.dismiss(animated: true, completion: nil)
+            }
+            return
+        }
+        
         let newCredentials = Credentials(password: newPasswordTextField.text!)
         let newPassword = newCredentials.password.data(using: .utf8)!
         // kSecClass as String: kSecClassGenericPassword -> 이건 필요 없음
@@ -48,10 +58,11 @@ class PasswordChangeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        newPasswordTextField.delegate = self
         currentPasswordTextField.placeholder = retrieveCurrentPassword()
         
         // 새로운 비밀번호를 입력하지 않으면 버튼이 비활성화 상태(disabled state)로 대기하도록
-//        changePasswordButton.isEnabled = false
+        changePasswordButton.isEnabled = false
     }
     
     private func retrieveCurrentPassword() -> String {
@@ -74,5 +85,17 @@ class PasswordChangeViewController: UIViewController {
         }
         
         return password
+    }
+    
+    // 아래 메서드는 textField 값이 변경되기 직전마다 불린다.
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        // ⭐️ textField 가 비어있다면, 버튼을 비활성화시키고 리턴해라!
+        guard newPasswordTextField.text!.isEmpty == false else {
+            changePasswordButton.isEnabled = false
+            return
+        }
+        
+        // textField 가 수정을 시작했고 비어있지 않다면, 버튼을 활성화(enable) 시켜라!
+        changePasswordButton.isEnabled = true
     }
 }
